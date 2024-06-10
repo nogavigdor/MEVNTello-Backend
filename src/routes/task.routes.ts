fb0bc346ca40b77c0a501bf5d9ea76837691a287
Noga Vigdor <noga.vigdor@gmail.com>
2024-06-10 18:49:49 +0200

    updated routes with appropriate roles authorization

--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -2,15 +2,19 @@ import express from 'express';
 import { Types } from 'mongoose';
 import { verifyToken, isLeader, isTaskMember, taskValidation } from '../validation';
 import Task from '../models/task';
+import List from '../models/list';
 import { RequestHandler } from 'express';
 import  ISubTask  from '../interfaces/ISubTask';
+import { CustomRequest } from '../interfaces/ICustomRequest';
 
 const router = express.Router();
 
-// Get all tasks
+
+// Get all tasks assigned to the authenticated user
 router.get('/', verifyToken as RequestHandler, async (req, res) => {
+    const customReq = req as CustomRequest;
     try {
-        const tasks = await Task.find();
+        const tasks = await Task.find({ assignedMembers: customReq.user._id });
         res.json(tasks);
     } catch (err: unknown) {
         if (err instanceof Error) {
@@ -21,13 +25,29 @@ router.get('/', verifyToken as RequestHandler, async (req, res) => {
     }
 });
 
-// Create a new task
-router.post('/', verifyToken as RequestHandler, isLeader, async (req, res) => {
-    const { error } = taskValidation(req.body);
-    if (error) return res.status(400).json({ message: error.details[0].message });
+// Create a new task (only by leaders)
+router.post('/:listId', verifyToken as RequestHandler, async (req, res, next) => {
+    const customReq = req as CustomRequest;
+    const listId = req.params.listId;
 
-    const task = new Task(req.body);
     try {
+        const list = await List.findById(listId);
+        if (!list) return res.status(404).json({ message: 'List not found' });
+
+        // Add projectId to the body for isLeader middleware
+        req.body.projectId = list.projectId;
+
+        // Verify if the user is a leader of the project
+        await isLeader(req, res, next);
+
+        const { error } = taskValidation(req.body);
+        if (error) return res.status(400).json({ message: error.details[0].message });
+
+        const task = new Task({
+            ...req.body,
+            listId: listId
+        });
+
         const savedTask = await task.save();
         res.status(201).json(savedTask);
     } catch (err: unknown) {
@@ -39,48 +59,78 @@ router.post('/', verifyToken as RequestHandler, isLeader, async (req, res) => {
     }
 });
 
-// Update a task
-router.put('/:id', verifyToken as RequestHandler, async (req, res) => {
+// Update a task - id is the task ID, only task members can update hoursUsed, other updates are allowed only for the leader
+router.put('/:id', verifyToken as RequestHandler, async (req, res, next) => {
+    const customReq = req as CustomRequest;
     const { error } = taskValidation(req.body);
     if (error) return res.status(400).json({ message: error.details[0].message });
 
-    // Check if the update includes hoursUsed
-    if ('hoursUsed' in req.body) {
-        // Only task members can update hoursUsed
-        await isTaskMember(req, res, async () => {
-            try {
-                const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
-                if (!updatedTask) return res.status(404).json({ message: "Task not found" });
-                res.json(updatedTask);
-            } catch (err: unknown) {
-                if (err instanceof Error) {
-                    res.status(400).json({ message: err.message });
-                } else {
-                    res.status(400).json({ message: 'An unknown error occurred' });
+    try {
+        const task = await Task.findById(req.params.id);
+        if (!task) return res.status(404).json({ message: 'Task not found' });
+
+        const list = await List.findById(task.listId);
+        if (!list) return res.status(404).json({ message: 'List not found' });
+
+        // Add projectId to the body for isLeader middleware
+        req.body.projectId = list.projectId;
+
+        // Check if the update includes hoursUsed
+        if ('hoursUsed' in req.body) {
+            // Only task members can update hoursUsed
+            await isTaskMember(req, res, async () => {
+                try {
+                    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+                    if (!updatedTask) return res.status(404).json({ message: "Task not found" });
+                    res.json(updatedTask);
+                } catch (err: unknown) {
+                    if (err instanceof Error) {
+                        res.status(400).json({ message: err.message });
+                    } else {
+                        res.status(400).json({ message: 'An unknown error occurred' });
+                    }
                 }
-            }
-        });
-    } else {
-        // Other updates are allowed only for the leader
-        await isLeader(req, res, async () => {
-            try {
-                const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
-                if (!updatedTask) return res.status(404).json({ message: "Task not found" });
-                res.json(updatedTask);
-            } catch (err: unknown) {
-                if (err instanceof Error) {
-                    res.status(400).json({ message: err.message });
-                } else {
-                    res.status(400).json({ message: 'An unknown error occurred' });
+            });
+        } else {
+            // Other updates are allowed only for the leader
+            await isLeader(req, res, async () => {
+                try {
+                    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+                    if (!updatedTask) return res.status(404).json({ message: "Task not found" });
+                    res.json(updatedTask);
+                } catch (err: unknown) {
+                    if (err instanceof Error) {
+                        res.status(400).json({ message: err.message });
+                    } else {
+                        res.status(400).json({ message: 'An unknown error occurred' });
+                    }
                 }
-            }
-        });
+            });
+        }
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            res.status(500).json({ message: err.message });
+        } else {
+            res.status(500).json({ message: 'An unknown error occurred' });
+        }
     }
 });
 
 // Delete a task
-router.delete('/:id', verifyToken as RequestHandler, isLeader, async (req, res) => {
+router.delete('/:id', verifyToken as RequestHandler, async (req, res, next) => {
+    const customReq = req as CustomRequest;
+
     try {
+        const task = await Task.findById(req.params.id);
+        if (!task) return res.status(404).json({ message: 'Task not found' });
+
+        const list = await List.findById(task.listId);
+        if (!list) return res.status(404).json({ message: 'List not found' });
+
+        // Add projectId to the body for isLeader middleware
+        req.body.projectId = list.projectId;
+        await isLeader(req, res, next);
+
         const removedTask = await Task.findByIdAndDelete(req.params.id);
         if (!removedTask) return res.status(404).json({ message: "Task not found" });
         res.json({ message: "Task deleted" });
@@ -184,6 +234,7 @@ router.delete('/:taskId/subtasks/:subtaskId', verifyToken as RequestHandler, isT
             res.status(400).json({ message: 'An unknown error occurred' });
         }
     }
-});
+}
+);
 
 export default router;
