01c4068cf3dd7b239de597607110031054f159d3
Noga Vigdor <noga.vigdor@gmail.com>
2024-07-29 20:12:47 +0200

    updating project validation to have an optional lists property

--- src/validation.ts
+++ src/validation.ts
@@ -28,6 +28,7 @@ const projectValidation = (data: ProjectDocument) => {
                 role: Joi.string().valid('leader', 'member').required()
             })
         ).min(1),
+        lists : Joi.array().items(Joi.string()).optional(),
         createdAt: Joi.date().optional(),
         updatedAt: Joi.date().optional()
     });
